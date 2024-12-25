00caf78318e8180254c9b0586a24dfd17f4fe598
sudan <sudosuwal@gmail.com>
2024-12-26 02:44:47 +1100

    fix: remove console

--- src/figma/index.ts
+++ src/figma/index.ts
@@ -3,7 +3,6 @@ import { ReactNativeNode } from "../generator/react-native/react-native-node";
 export function getSelectedReactNode() {
     const node = figma.currentPage.selection[0];
     const reactNode = new ReactNativeNode(node)
-    console.log(node.exportAsync())
     return reactNode;
 }
 
